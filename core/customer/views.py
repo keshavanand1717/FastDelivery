5cece8aeb60793c148c5d93332bd1df659ad2748
Anand Keshav <95139377+keshavanand1717@users.noreply.github.com>
2023-06-16 12:37:37 +0530

    Added my project

---
+++ core/customer/views.py
@@ -0,0 +1,124 @@
+import stripe
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+from core.customer import forms
+from django.contrib import messages
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.conf import settings
+
+stripe.api_key = settings.STRIPE_API_SECRET_KEY
+
+# Create your views here.
+@login_required()
+def home(request):
+    return redirect(reverse('customer:profile'))
+
+@login_required(login_url="sign-in/?next=/customer/")
+def profile_page(request):
+    user_form = forms.BasicUserForm(instance=request.user)
+    customer_form = forms.BasicCustomerForm(instance=request.user.customer)
+    password_form = PasswordChangeForm(request.user,request.POST)
+    if request.method == "POST":
+        if request.POST.get('action') == 'update_password':
+            if password_form.is_valid():
+                user = password_form.save()
+                update_session_auth_hash(request,user)
+
+                messages.success(request, 'Your password has been updated')
+                return redirect(reverse('customer:profile'))
+
+        if request.POST.get('action') == 'update_profile':
+            user_form = forms.BasicUserForm(request.POST, instance=request.user)
+            customer_form = forms.BasicCustomerForm(request.POST,request.FILES,instance=request.user.customer)
+            if user_form.is_valid() and customer_form.is_valid():
+                user_form.save()
+                customer_form.save()
+                messages.success(request, 'Your profile has been updated')
+                return redirect(reverse('customer:profile'))
+        
+    return render(request,'customer/profile.html',{
+            "user_form": user_form,
+            "customer_form": customer_form,
+            "password_form": password_form
+    })
+
+@login_required(login_url="sign-in/?next=/customer/")
+def payment_method_page(request):
+    current_customer = request.user.customer
+
+    # Save stripe customer info
+    if not current_customer.stripe_customer_id:
+        customer = stripe.Customer.create()
+        current_customer.stripe_customer_id = customer['id']
+        current_customer.save()
+
+    #Get Stripe payment method
+    stripe_payment_methods = stripe.PaymentMethod.list(
+        customer = current_customer.stripe_customer_id,
+        type = "card",
+    )
+
+    print(stripe_payment_methods)
+
+    if stripe_payment_methods and len(stripe_payment_methods.data) > 0:
+        payment_method = stripe_payment_methods.data[0]
+        current_customer.stripe_payment_method_id = payment_method.id
+        current_customer.stripe_card_last4 = payment_method.card.last4
+        current_customer.save()
+    else:
+        current_customer.stripe_payment_method_id = ""
+        current_customer.stripe_card_last4 = ""
+        current_customer.save()   
+    intent = stripe.SetupIntent.create(
+        customer = current_customer.stripe_customer_id
+    )
+
+    return render(request,'customer/payment_method.html',{
+        "client_secret": intent.client_secret,
+        "STRIPE_API_PUBLIC_KEY": settings.STRIPE_API_PUBLIC_KEY,
+    })
+
+@login_required(login_url="sign-in/?next=/customer/")
+def create_job_page(request):
+   
+    current_customer = request.user.customer
+     # if not request.user.customer.stripe_payment_method_id:
+    #     return redirect(reverse('customer:payment_method')) 
+    
+    # creating_job = Job.objects.filter(customer=current_customer, status = Job.CREATING_STATUS).
+    #SOME debugging req.
+    creating_job = forms.Job.objects.filter(customer=current_customer, status = forms.Job.CREATING_STATUS).last()
+    step1_form = forms.JobCreateStep1Form(instance=creating_job)
+    step2_form = forms.JobCreateStep2Form(instance=creating_job)
+    step3_form = forms.JobCreateStep3Form(instance=creating_job)
+
+    if request.method == "POST":
+        if request.POST.get('step') == '1':
+            step1_form = forms.JobCreateStep1Form(request.POST,request.FILES,instance = creating_job)
+            if step1_form.is_valid(): 
+                creating_job = step1_form.save(commit=False)
+                creating_job.customer = current_customer
+                creating_job.save()
+                return redirect(reverse('customer:create_job'))
+        elif request.POST.get('step') == '2':
+            step2_form = forms.JobCreateStep2Form(request.POST, instance=creating_job)
+            if step2_form.is_valid():
+                creating_job = step2_form.save()
+                return redirect(reverse('customer:create_job'))
+    #Determine the current step
+    if not creating_job:
+        current_step = 1
+    elif creating_job.pickup_name:
+        current_step = 3    
+    else:
+        current_step = 2
+
+    return render(request, 'customer/create_job.html',{
+         "job":creating_job,
+         "step":current_step,
+         "step1_form": step1_form,
+         "step2_form": step2_form,
+         "step3_form": step3_form,
+    })
---
+++ fastdelivery/urls.py
@@ -0,0 +1,36 @@
+from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from core import views
+from core.customer import views as customer_views
+from core.courier import views as courier_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+cutomer_urlpatterns = [
+    path('',customer_views.home,name="home"),
+    path('profile/',customer_views.profile_page,name="profile"),
+    path('payment/',customer_views.payment_method_page,name="payment"),
+    path('create_job/',customer_views.create_job_page,name="create_job"),
+    
+]
+courier_urlpatterns = [
+    path('',courier_views.home,name="home"),
+    
+
+]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+
+    path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html")),
+    path('sign-out/', auth_views.LogoutView.as_view(next_page="/")),
+    path('sign-up/',views.sign_up),
+
+    path('customer/', include((cutomer_urlpatterns, 'customer'))),
+    path('courier/', include((courier_urlpatterns, 'courier'))),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
